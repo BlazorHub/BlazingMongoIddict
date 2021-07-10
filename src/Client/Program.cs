69c72bd02fe444e7df09dcdaafe8a3e781012ca5
Brian Buvinghausen <buvinghausen@users.noreply.github.com>
2021-07-10 11:58:49 -0500

    Client cleanup

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,19 +1,15 @@
-using System;
-using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace BlazingMongoIddict.Client
 {
-    public class Program
+	internal class Program
     {
-        public static async Task Main(string[] args)
+        private static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
