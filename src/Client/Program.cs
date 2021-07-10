94f60979bb1a5ee0f686741a9998fd368a94293c
Brian Buvinghausen <buvinghausen@users.noreply.github.com>
2021-07-10 12:05:06 -0500

    Finish client cleanup

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,28 +1,31 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BlazingMongoIddict.Client
 {
 	internal class Program
-    {
-        private static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+	{
+		private static Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddHttpClient("BlazingMongoIddict.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+			builder.Services
+				.AddHttpClient("BlazingMongoIddict.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+				.AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
-            // Supply HttpClient instances that include access tokens when making requests to the server project
-            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazingMongoIddict.ServerAPI"));
+			// Supply HttpClient instances that include access tokens when making requests to the server project
+			builder.Services
+				.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazingMongoIddict.ServerAPI"))
+				.AddApiAuthorization();
 
-            builder.Services.AddApiAuthorization();
-
-            await builder.Build().RunAsync();
-        }
-    }
+			return builder
+				.Build()
+				.RunAsync();
+		}
+	}
 }
